fd6b323fe02a41023885bb0a2afdfdd9bc5b759b
Omar Rendon <omarcr.96@gmail.com>
2025-01-23 21:47:43 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { RenderProps } from "./components";
+
+function App() {
+  return (
+    <>
+      <RenderProps />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/RenderProps/RenderProps.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RenderProps = () => {
+  return <div>RenderProps</div>;
+};
